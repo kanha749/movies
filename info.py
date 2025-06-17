82dd18ff2d3738b81b20d0309fedcbf73fb9fdce
kanha749 <kanhapareek112@gmail.com>
2025-06-17 12:51:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,11 +18,11 @@ API_HASH = environ.get('API_HASH', '1560930c983fbca6a1fcc8eab760d40d')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7584097532:AAHmktfV7r5WWFA6x5YZY6-FzsTOeX2p9c8')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1840465305').split()]
+USERNAME = environ.get('USERNAME', "") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+847aMqDEJO9lMjQ1')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002155332774').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kanha123:kanha%40123@cluster0.4oucl36.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
